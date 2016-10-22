bb8d55b9e15bb3a04580b353761aba7f964ca993
Mark Swift <mark@useprofile.com>
2016-10-22 17:07:52 +0100

    Revert "Merge branch 'master' into pr/2"
    This reverts commit c21bc68458d276e3aef45a2442502441e6dbe1e4.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict'
 var FlowActions = require('./lib/flow/actions.js')
+
 var self = module.exports = { // eslint-disable-line
-	init: function() {
-			console.log('Initialising LaMetric app...')
-			FlowActions.init()
-		} // end of module init function
+  init: function () {
+    console.log('Initialising LaMetric app...')
+    FlowActions.init()
+  } // end of module init function
 }
